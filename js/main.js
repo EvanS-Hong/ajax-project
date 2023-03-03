12e59152269fa8f55f9443169c65c2d8499619e0
Evan Hong <e.hong1424@gmail.com>
2023-03-03 00:57:54 +0000

    WIP /SAVING #3

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ xhr.addEventListener('load', function () {
   for (var i = 0; i < xhr.response.data.length; i++) {
     var div = document.createElement('div');
     div.className = 'character';
-
     div.classList.add(xhr.response.data[i].role.displayName.toLowerCase());
+    div.classList.add(xhr.response.data[i].displayName.toLowerCase());
 
     var img1 = document.createElement('img');
     img1.src = xhr.response.data[i].displayIcon;
@@ -41,7 +41,7 @@ xhr.addEventListener('load', function () {
     }
 
     var div3 = document.createElement('div');
-    div3.className = '.role-wrapper';
+    div3.className = 'role-wrapper';
 
     var text2 = document.createElement('h2');
     text2.textContent = '// ROLE';
@@ -59,6 +59,65 @@ xhr.addEventListener('load', function () {
     var text4 = document.createElement('p');
     text4.textContent = xhr.response.data[i].description;
 
+    var text5 = document.createElement('p');
+    text5.textContent = xhr.response.data[i].role.description;
+
+    var div4 = document.createElement('div');
+    div4.className = 'abilities';
+
+    var img5 = document.createElement('img');
+    img5.src = xhr.response.data[i].abilities[0].displayIcon;
+    img5.alt = xhr.response.data[i].abilities[0].displayName;
+    img5.className = 'ability';
+
+    var img6 = document.createElement('img');
+    img6.src = xhr.response.data[i].abilities[1].displayIcon;
+    img6.alt = xhr.response.data[i].abilities[1].displayName;
+    img6.className = 'ability';
+
+    var img7 = document.createElement('img');
+    img7.src = xhr.response.data[i].abilities[2].displayIcon;
+    img7.alt = xhr.response.data[i].abilities[2].displayName;
+    img7.className = 'ability';
+
+    var img8 = document.createElement('img');
+    img8.src = xhr.response.data[i].abilities[3].displayIcon;
+    img8.alt = xhr.response.data[i].abilities[3].displayName;
+    img8.className = 'ability';
+
+    var div5 = document.createElement('div');
+    div5.className = 'info-window';
+
+    var div6 = document.createElement('div');
+    div6.className = 'main-window';
+
+    var div7 = document.createElement('div');
+    div7.className = 'exit';
+
+    var p1 = document.createElement('p');
+    p1.textContent = '&#10006;';
+
+    var div8 = document.createElement('div');
+    div8.className = 'row';
+
+    var div9 = document.createElement('div');
+    div9.className = 'column';
+
+    var div10 = document.createElement('div');
+    div10.className = 'column';
+
+    var div11 = document.createElement('div');
+    div11.className = 'row';
+
+    var div12 = document.createElement('div');
+    div12.className = 'abilities';
+
+    var div13 = document.createElement('div');
+    div13.className = 'ability-row';
+
+    var div14 = document.createElement('div');
+    div14.className = 'ability-row';
+
     desc.appendChild(div2);
     div2.appendChild(text2);
     div2.appendChild(div3);
@@ -66,7 +125,30 @@ xhr.addEventListener('load', function () {
     div3.appendChild(img3);
     div2.appendChild(text3);
     div2.appendChild(text4);
-
+    div2.appendChild(div4);
+    div4.appendChild(img5);
+    div4.appendChild(img6);
+    div4.appendChild(img7);
+    div4.appendChild(img8);
+    document.body.appendChild(div5);
+    document.body.appendChild(div6);
+    div6.appendChild(div7);
+    div6.appendChild(div8);
+    div8.appendChild(div9);
+    div9.appendChild(img3);
+    div8.appendChild(div10);
+    div10.appendChild(text);
+    div10.appendChild(h1);
+    div10.appendChild(text5);
+    div6.appendChild(div11);
+    div11.appendChild(text4);
+    div6.appendChild(div12);
+    div12.appendChild(div13);
+    div13.appendChild(img5);
+    div13.appendChild(img6);
+    div12.appendChild(div14);
+    div14.appendChild(img7);
+    div14.appendChild(img8);
   }
 });
 xhr.send();
